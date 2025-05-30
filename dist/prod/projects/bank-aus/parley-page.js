7349447740babb693054d6003f82249a5279971f
Paras Shah <parasshah195@gmail.com>
2025-05-30 11:51:45 +0530

    Initialise scroll smoother after webflow loads

--- dist/prod/projects/bank-aus/parley-page.js
+++ dist/prod/projects/bank-aus/parley-page.js
@@ -1,1 +1,1 @@
-"use strict";(()=>{ScrollSmoother.create({wrapper:"#smooth-wrapper",content:"#smooth-content",smooth:1.2,effects:!1});})();
+"use strict";(()=>{window.Webflow||=[];window.Webflow.push(()=>{ScrollSmoother.create({wrapper:"#smooth-wrapper",content:"#smooth-content",smooth:1.2,effects:!1})});})();
--- src/projects/bank-aus/parley-page.ts
+++ src/projects/bank-aus/parley-page.ts
@@ -1,6 +1,9 @@
-ScrollSmoother.create({
-  wrapper: '#smooth-wrapper',
-  content: '#smooth-content',
-  smooth: 1.2,
-  effects: false, // set to true if you want to use data-speed etc.
+window.Webflow ||= [];
+window.Webflow.push(() => {
+  ScrollSmoother.create({
+    wrapper: '#smooth-wrapper',
+    content: '#smooth-content',
+    smooth: 1.2,
+    effects: false, // set to true if you want to use data-speed etc.
+  });
 });
